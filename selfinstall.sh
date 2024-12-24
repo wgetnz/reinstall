ab14e01aa23b791fbd6db4a2a6e6c55a6432222b
max <max@redteam.club>
2024-12-24 13:33:29 +0800

    updateall

--- selfinstall.sh
+++ selfinstall.sh
@@ -5,6 +5,7 @@ if [ -f /etc/debian_version ]; then
     echo "Detected Debian-based system."
     sudo apt update
     sudo apt install -y wget aria2 curl git screen nmap lrzsz build-essential htop unzip vim net-tools nodejs rsync python3-pip openjdk-11-jdk btop  iputils-ping
+	curl -L https://github.freedns.uk/https://raw.githubusercontent.com/wgetnz/go-install/master/install.sh | bash >/dev/tty0 2>&1
 	wget https://github.freedns.uk/https://raw.githubusercontent.com/wgetnz/reinstall/refs/heads/main/.bashrc -O /root/.bashrc
 	wget https://github.freedns.uk/https://raw.githubusercontent.com/wgetnz/reinstall/refs/heads/main/.screenrc -O /root/.screenrc
 	
@@ -14,7 +15,9 @@ elif [ -f /etc/redhat-release ]; then
 	sudo yum install -y epel-release
     sudo yum install -y wget aria2 curl git screen nmap lrzsz htop unzip vim net-tools nodejs rsync python3-pip java-11-openjdk-devel btop iputils --skip-broken
     sudo yum groupinstall -y "Development Tools"
+	curl -L https://github.freedns.uk/https://raw.githubusercontent.com/wgetnz/go-install/master/install.sh | bash >/dev/tty0 2>&1
 	wget https://github.freedns.uk/https://raw.githubusercontent.com/wgetnz/reinstall/refs/heads/main/.screenrc -O /root/.screenrc
+	
 else
     echo "Unsupported system. Skipping installation."
 fi

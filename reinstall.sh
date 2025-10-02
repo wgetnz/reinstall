a38f7066b0510bbb80f46f6f2c3936709676230b
maxowner <max@redteam.club>
2025-10-02 14:41:30 +0800

    updateok

--- reinstall.sh
+++ reinstall.sh
@@ -3,8 +3,8 @@
 # shellcheck disable=SC2086
 
 set -eE
-confhome=https://raw.githubusercontent.com/bin456789/reinstall/main
-confhome_cn=https://cnb.cool/bin456789/reinstall/-/git/raw/main
+confhome=https://raw.githubusercontent.com/wgetnz/reinstall/main
+confhome_cn=https://github.freedns.uk/https://raw.githubusercontent.com/wgetnz/reinstall/main
 # confhome_cn=https://www.ghproxy.cc/https://raw.githubusercontent.com/bin456789/reinstall/main
 
 # 默认密码

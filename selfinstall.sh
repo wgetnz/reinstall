4ed4cc149d0d76c979f9f717ca97de141db32a76
maxowner <max@redteam.club>
2026-02-25 12:58:48 +0800

    fix(selfinstall): 安装前更新系统软件包

--- selfinstall.sh
+++ selfinstall.sh
@@ -3,8 +3,8 @@
 # 检查是否为Debian系列
 if [ -f /etc/debian_version ]; then
     echo "Detected Debian-based system."
-    sudo apt update
-    sudo apt install -y wget aria2 curl git screen nmap lrzsz build-essential htop unzip vim net-tools nodejs rsync python3-pip openjdk-11-jdk btop  iputils-ping lrzsz  
+    sudo apt update && sudo apt upgrade -y
+    sudo apt install -y wget aria2 curl git screen nmap lrzsz build-essential htop unzip vim net-tools nodejs rsync python3-pip openjdk-11-jdk btop  iputils-ping lrzsz
 	curl -L https://github.freedns.uk/https://raw.githubusercontent.com/wgetnz/go-install/master/install.sh | bash >/dev/tty0 2>&1
 	wget https://github.freedns.uk/https://raw.githubusercontent.com/wgetnz/reinstall/refs/heads/main/.bashrc -O /root/.bashrc
 	wget https://github.freedns.uk/https://raw.githubusercontent.com/wgetnz/reinstall/refs/heads/main/.screenrc -O /root/.screenrc
@@ -17,6 +17,7 @@ if [ -f /etc/debian_version ]; then
 # 检查是否为RedHat系列
 elif [ -f /etc/redhat-release ]; then
     echo "Detected RedHat-based system."
+	sudo yum update -y
 	sudo yum install -y epel-release
     sudo yum install -y wget aria2 curl git screen nmap lrzsz htop unzip vim net-tools nodejs rsync python3-pip java-11-openjdk-devel btop iputils lrzsz --skip-broken
     sudo yum groupinstall -y "Development Tools"

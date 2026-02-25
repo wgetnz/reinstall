4fe54f563412b086489c7e6e9f58dd5c77d756cd
maxowner <max@redteam.club>
2026-02-25 13:15:54 +0800

    fix(selfinstall): 禁用 EDD 探测，消除开机 Probing EDD 提示

--- selfinstall.sh
+++ selfinstall.sh
@@ -8,6 +8,9 @@ if [ -f /etc/debian_version ]; then
 	curl -L https://github.freedns.uk/https://raw.githubusercontent.com/wgetnz/go-install/master/install.sh | bash >/dev/tty0 2>&1
 	wget https://github.freedns.uk/https://raw.githubusercontent.com/wgetnz/reinstall/refs/heads/main/.bashrc -O /root/.bashrc
 	wget https://github.freedns.uk/https://raw.githubusercontent.com/wgetnz/reinstall/refs/heads/main/.screenrc -O /root/.screenrc
+    # 禁用 EDD 探测，消除开机 "Probing EDD" 提示
+    sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="\(.*\)"/GRUB_CMDLINE_LINUX_DEFAULT="\1 edd=off"/' /etc/default/grub
+    update-grub 2>/dev/null || true
     systemctl stop systemd-resolved
 	systemctl disable systemd-resolved
  	rm -rf /etc/resolv.conf
@@ -24,7 +27,10 @@ elif [ -f /etc/redhat-release ]; then
 	curl -L https://github.freedns.uk/https://raw.githubusercontent.com/wgetnz/go-install/master/install.sh | bash >/dev/tty0 2>&1
 	wget https://github.freedns.uk/https://raw.githubusercontent.com/wgetnz/reinstall/refs/heads/main/.bashrc -O /root/.bashrc
 	wget https://github.freedns.uk/https://raw.githubusercontent.com/wgetnz/reinstall/refs/heads/main/.screenrc -O /root/.screenrc
-	
+	# 禁用 EDD 探测，消除开机 "Probing EDD" 提示
+	sed -i 's/GRUB_CMDLINE_LINUX="\(.*\)"/GRUB_CMDLINE_LINUX="\1 edd=off"/' /etc/default/grub
+	grub2-mkconfig -o /boot/grub2/grub.cfg 2>/dev/null || true
+
 else
     echo "Unsupported system. Skipping installation."
 fi
